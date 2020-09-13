5befdc1ded427b83136d687445aea76b7f224151
compumike08 <compumike08@users.noreply.github.com>
2020-09-13 13:41:09 -0400

    Add initial skeleton spring project

---
+++ src/main/java/mh/michael/monopolybanking/MonopolyBankingApplication.java
@@ -0,0 +1,13 @@
+package mh.michael.monopolybanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mh/michael/monopolybanking/MonopolyBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package mh.michael.monopolybanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
