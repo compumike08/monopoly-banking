d364335a6f20230384dec613f81b394ffb719eb8
compumike08 <compumike08@users.noreply.github.com>
2020-09-20 16:35:46 -0400

    Add check for multiple bankers per game when creating new user

--- src/main/java/mh/michael/monopolybanking/service/UserService.java
+++ src/main/java/mh/michael/monopolybanking/service/UserService.java
@@ -5,12 +5,15 @@ import mh.michael.monopolybanking.dto.NewUserRequestDTO;
 import mh.michael.monopolybanking.dto.UserDTO;
 import mh.michael.monopolybanking.model.Game;
 import mh.michael.monopolybanking.model.User;
+import mh.michael.monopolybanking.model.UserRole;
 import mh.michael.monopolybanking.repository.GameRepository;
 import mh.michael.monopolybanking.repository.UserRepository;
 import mh.michael.monopolybanking.util.ConvertDTOUtil;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,14 +33,21 @@ public class UserService {
 
     @Transactional
     public UserDTO createNewUser(long gameId, NewUserRequestDTO newUserRequestDTO) {
+        UserRole newUserRole = newUserRequestDTO.getUserRole();
         Game game = gameRepository.getOne(gameId);
+
+        if (newUserRole.equals(UserRole.BANKER) && game.getUsers().stream().anyMatch(user -> user.getUserRole().equals(UserRole.BANKER))) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "This game already has a banker");
+        }
+
         User newUser = User.builder()
                 .game(game)
-                .userRole(newUserRequestDTO.getUserRole())
+                .userRole(newUserRole)
                 .moneyBalance(STARTING_MONEY_AMT)
                 .name(newUserRequestDTO.getName())
                 .code(RandomStringUtils.randomAlphanumeric(CODE_LENGTH))
                 .build();
+
         User updatedUser = userRepository.save(newUser);
 
         return ConvertDTOUtil.convertUserToUserDTO(updatedUser);
