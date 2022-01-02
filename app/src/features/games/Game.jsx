4ca03ebb3d4bfefe296104394adb4db60784bb9a
compumike08 <compumike08@users.noreply.github.com>
2022-01-02 15:25:26 -0500

    Remove gameId from Game component

--- app/src/features/games/Game.jsx
+++ app/src/features/games/Game.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { Card, CardBody, CardTitle } from "reactstrap";
 
-const Game = ({ gameId, code }) => {
+const Game = ({ code }) => {
     return (
         <Card>
             <CardBody>
                 <CardTitle tag="h5">
                     {code}
                 </CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                    {gameId}
-                </CardSubtitle>
             </CardBody>
         </Card>
     );
 }
 
 Game.propTypes = {
-    gameId: PropTypes.number.isRequired,
     code: PropTypes.string.isRequired
 };
 
