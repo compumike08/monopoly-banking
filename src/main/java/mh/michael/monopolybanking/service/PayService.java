ce9a4a573b1d7823aa3d708bbf18af33aeede8d8
compumike08 <compumike08@users.noreply.github.com>
2022-09-08 02:04:40 -0400

    Add backend check to ensure only the banker can pay from a money sink

--- src/main/java/mh/michael/monopolybanking/service/PayService.java
+++ src/main/java/mh/michael/monopolybanking/service/PayService.java
@@ -5,6 +5,7 @@ import mh.michael.monopolybanking.dto.PayRequestDTO;
 import mh.michael.monopolybanking.dto.PayResponseDTO;
 import mh.michael.monopolybanking.model.MoneySink;
 import mh.michael.monopolybanking.model.User;
+import mh.michael.monopolybanking.model.UserRole;
 import mh.michael.monopolybanking.repository.MoneySinkRepository;
 import mh.michael.monopolybanking.repository.UserRepository;
 import org.springframework.http.HttpStatus;
@@ -49,6 +50,14 @@ public class PayService {
         User toUser = null;
 
         if (payRequestDTO.isFromSink()) {
+            User requesterUser = userRepository.getOne(payRequestDTO.getRequestInitiatorUserId());
+
+            if (!requesterUser.getUserRole().equals(UserRole.BANKER)) {
+                String errMsg = "Only the banker can pay from a money sink";
+                log.error(errMsg);
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, errMsg);
+            }
+
             fromMoneySink = moneySinkRepository.getOne(payRequestDTO.getFromId());
 
             if (fromMoneySink.getMoneyBalance() != payRequestDTO.getOriginalFromAmount()) {
