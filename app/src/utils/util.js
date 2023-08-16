2c739c0a88cff174c3c4e9aa2fc42d421d31f014
compumike08 <compumike08@users.noreply.github.com>
2023-08-16 19:56:07 -0400

    Disable eslint no-useless-escape for line in util.js that defines regex

--- app/src/utils/util.js
+++ app/src/utils/util.js
@@ -8,6 +8,7 @@ export const formatNumberAsCurrency = number => {
 
 export const isEmailValid = email => {
     // JavaScript regular expression to validate email from https://emailregex.com/index.html
+    // eslint-disable-next-line no-useless-escape
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
 };
