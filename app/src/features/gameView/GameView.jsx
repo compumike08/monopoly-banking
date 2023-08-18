1e9dc18568e44150c27096cd619647362226eca5
compumike08 <compumike08@users.noreply.github.com>
2023-08-18 13:22:20 -0400

    Refactor GameView to use tabs

--- app/src/features/gameView/GameView.jsx
+++ app/src/features/gameView/GameView.jsx
@@ -1,24 +1,19 @@
 import React, { PureComponent } from "react";
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Container, Row, Col } from "reactstrap";
-import { BANKER_ROLE } from "../../constants/general";
-import PlayerCard from '../../sharedComponents/PlayerCard';
-import MoneySinkCard from "../../sharedComponents/MoneySinkCard";
-import PayPlayerFromSinkButton from "../../sharedComponents/PayPlayerFromSinkButton";
-import PaymentRecords from "../payments/PaymentRecords";
-import { sendPaymentAction, getAllPaymentsAction } from "../games/gamesSlice";
-import { selectActiveGamePlayersYouOnTop, selectLoggedInPlayer } from "./gamePlayersSelector";
-import { selectActiveGameBankMoneySinkOnTop } from "./gameMoneySinksSelector";
+import { Container, Row, Col, Nav, NavItem, NavLink, TabContent, TabPane } from "reactstrap";
+import PaymentTabView from "./PaymentTabView";
 
 class GameView extends PureComponent {
-    componentDidMount() {
-        this.props.actions.getAllPaymentsAction(this.props.gameId);
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeTabId: "1"
+        };
     }
 
     render() {
-        const { loggedInPlayerId, loggedInPlayerObject, players, gameCode, gameId, moneySinks, paymentRecords } = this.props;
-        const { playerRole } = loggedInPlayerObject;
+        const { gameCode } = this.props;
 
         return (
             <Container>
@@ -29,68 +24,22 @@ class GameView extends PureComponent {
                         </h2>
                     </Col>
                 </Row>
-                {playerRole === BANKER_ROLE && (
-                    <Row>
-                        <Col>
-                            <PayPlayerFromSinkButton />
-                        </Col>
-                    </Row>
-                )}
                 <Row>
-                    <Col lg="4">
-                        <Row>
-                            <Col>
-                                <h3>Players</h3>
-                            </Col>
-                        </Row>
-                        {players.map(player => {
-                            return (
-                                <Row key={`user-${player.id}`}>
-                                    <Col>
-                                        <PlayerCard
-                                            player={player}
-                                            gameId={gameId}
-                                            loggedInPlayerId={loggedInPlayerId}
-                                        />
-                                    </Col>
-                                </Row>
-                            );
-                        })}
-                    </Col>
-                    <Col lg="4">
-                        <Row>
-                            <Col>
-                                <h3>Money Sinks</h3>
-                            </Col>
-                        </Row>
-                        {moneySinks.map(moneySink => {
-                            return (
-                                <Row key={`sink-${moneySink.id}`}>
-                                    <Col>
-                                        <MoneySinkCard
-                                            gameId={gameId}
-                                            loggedInPlayerId={loggedInPlayerId}
-                                            sink={moneySink}
-                                        />
-                                    </Col>
-                                </Row>
-                            );
-                        })}
-                    </Col>
-                    <Col lg="4">
-                        <Row>
-                            <Col>
-                                <h3>Payment Records</h3>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <PaymentRecords
-                                    paymentRecords={paymentRecords}
-                                />
-                            </Col>
-                        </Row>
-                    </Col>
+                    <Nav tabs className="mb-3">
+                        <NavItem>
+                            <NavLink
+                                active={this.state.activeTabId === "1"}
+                                onClick={() => { this.setState({ activeTabId: "1" }); }}
+                            >
+                                Payments
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTabId}>
+                        <TabPane tabId="1">
+                            <PaymentTabView />
+                        </TabPane>
+                    </TabContent>
                 </Row>
             </Container>
         );
@@ -99,23 +48,8 @@ class GameView extends PureComponent {
 
 function mapStateToProps(state) {
     return {
-        gameId: state.gamesData.activeGame.gameId,
-        gameCode: state.gamesData.activeGame.code,
-        loggedInPlayerId: state.gamesData.activeGame.loggedInPlayerId,
-        players: selectActiveGamePlayersYouOnTop(state),
-        moneySinks: selectActiveGameBankMoneySinkOnTop(state),
-        loggedInPlayerObject: selectLoggedInPlayer(state),
-        paymentRecords: state.gamesData.activeGame.paymentRecords
+        gameCode: state.gamesData.activeGame.code
     };
 };
 
-function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators({
-            sendPaymentAction,
-            getAllPaymentsAction
-        }, dispatch)
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(GameView);
+export default connect(mapStateToProps)(GameView);
---
+++ app/src/features/gameView/PaymentTabView.jsx
@@ -0,0 +1,113 @@
+import React, { PureComponent } from "react";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Col, Row } from "reactstrap";
+import { BANKER_ROLE } from "../../constants/general";
+import PayPlayerFromSinkButton from "../../sharedComponents/PayPlayerFromSinkButton";
+import PlayerCard from "../../sharedComponents/PlayerCard";
+import MoneySinkCard from "../../sharedComponents/MoneySinkCard";
+import PaymentRecords from "../payments/PaymentRecords";
+import { getAllPaymentsAction } from "../games/gamesSlice";
+import { selectActiveGamePlayersYouOnTop, selectLoggedInPlayer } from "./gamePlayersSelector";
+import { selectActiveGameBankMoneySinkOnTop } from "./gameMoneySinksSelector";
+
+
+class PaymentTabView extends PureComponent {
+    componentDidMount() {
+        this.props.actions.getAllPaymentsAction(this.props.gameId);
+    }
+
+    render() {
+        const { loggedInPlayerId, loggedInPlayerObject, players, gameId, moneySinks, paymentRecords } = this.props;
+        const { playerRole } = loggedInPlayerObject;
+
+        return (
+            <>
+                {playerRole === BANKER_ROLE && (
+                    <Row>
+                        <Col>
+                            <PayPlayerFromSinkButton />
+                        </Col>
+                    </Row>
+                )}
+                <Row>
+                    <Col lg="4">
+                        <Row>
+                            <Col>
+                                <h3>Players</h3>
+                            </Col>
+                        </Row>
+                        {players.map(player => {
+                            return (
+                                <Row key={`user-${player.id}`}>
+                                    <Col>
+                                        <PlayerCard
+                                            player={player}
+                                            gameId={gameId}
+                                            loggedInPlayerId={loggedInPlayerId}
+                                        />
+                                    </Col>
+                                </Row>
+                            );
+                        })}
+                    </Col>
+                    <Col lg="4">
+                        <Row>
+                            <Col>
+                                <h3>Money Sinks</h3>
+                            </Col>
+                        </Row>
+                        {moneySinks.map(moneySink => {
+                            return (
+                                <Row key={`sink-${moneySink.id}`}>
+                                    <Col>
+                                        <MoneySinkCard
+                                            gameId={gameId}
+                                            loggedInPlayerId={loggedInPlayerId}
+                                            sink={moneySink}
+                                        />
+                                    </Col>
+                                </Row>
+                            );
+                        })}
+                    </Col>
+                    <Col lg="4">
+                        <Row>
+                            <Col>
+                                <h3>Payment Records</h3>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <PaymentRecords
+                                    paymentRecords={paymentRecords}
+                                />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </>
+        );        
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        gameId: state.gamesData.activeGame.gameId,
+        loggedInPlayerId: state.gamesData.activeGame.loggedInPlayerId,
+        players: selectActiveGamePlayersYouOnTop(state),
+        moneySinks: selectActiveGameBankMoneySinkOnTop(state),
+        loggedInPlayerObject: selectLoggedInPlayer(state),
+        paymentRecords: state.gamesData.activeGame.paymentRecords
+    };
+};
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            getAllPaymentsAction
+        }, dispatch)
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PaymentTabView);
